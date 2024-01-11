48f07fd70be74c3002f8f2d9ff3ba20cfd160849
leminhduc6402@gmail.com <leminhduc6402@gmail.com>
2024-01-11 15:19:08 +0700

    connect to database

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.useStaticAssets(join(__dirname, '..', 'public')); // access js, css, images
+  app.setBaseViewsDir(join(__dirname, '..', 'views')); // view
+  app.setViewEngine('ejs');
+
+  await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
