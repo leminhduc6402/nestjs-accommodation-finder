a5da11c5d8a209f10eac67e0121eb74d24f65d0f
leminhduc6402@gmail.com <leminhduc6402@gmail.com>
2024-05-19 10:23:07 +0700

    update upload file size

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -96,7 +96,6 @@ export class AuthService {
             role,
             permissions,
         } = user;
-        console.log(user);
         const payload = {
             sub: 'token login',
             iss: 'from server',
