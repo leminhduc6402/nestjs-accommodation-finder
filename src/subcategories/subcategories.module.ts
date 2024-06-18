bb6c0c66a66b023d13a09c25192b206c850a9e0a
leminhduc6402@gmail.com <leminhduc6402@gmail.com>
2024-06-18 23:17:42 +0700

    update

--- src/subcategories/subcategories.module.ts
+++ src/subcategories/subcategories.module.ts
@@ -3,16 +3,11 @@ import { SubcategoriesService } from './subcategories.service';
 import { SubcategoriesController } from './subcategories.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SubCategory, SubCategorySchema } from './schemas/subcategory.schema';
-import {
-    Category,
-    CategorySchema,
-} from 'src/categories/schemas/category.schema';
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: SubCategory.name, schema: SubCategorySchema },
-            { name: Category.name, schema: CategorySchema },
         ]),
     ],
     controllers: [SubcategoriesController],
